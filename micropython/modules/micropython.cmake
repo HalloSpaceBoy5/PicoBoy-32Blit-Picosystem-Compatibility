fb1971385838fe40a9a997ca85b0b032ad17bc21
HalloSpaceBoy <111699451+HalloSpaceBoy5@users.noreply.github.com>
2024-01-15 15:13:26 -0500

    Delete micropython directory

--- micropython/modules/micropython.cmake
+++
@@ -1,6 +0,0 @@
-include_directories(${CMAKE_CURRENT_LIST_DIR}/../../)
-
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
-
-include(picosystem/micropython)
--- micropython/modules_py/boot.py
+++
@@ -1,6 +0,0 @@
-# PicoSystem boot script
-# Runs the PicoSystem init() and launches the launcher if A is held.
-init()
-
-if button(A):
-    import launcher  # noqa: F401
